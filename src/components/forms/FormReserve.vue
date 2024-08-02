031d993e5595bfb4a9f59e831a3e04091d9f452e
LcDias <luis.carlos@vitta.me>
2024-08-02 19:06:37 -0300

    feat:personal data

--- src/components/forms/FormReserve.vue
+++ src/components/forms/FormReserve.vue
@@ -5,7 +5,17 @@
     &__hotel-name {
       font-size: 1.5rem;
     }
-    &__data {
+    &__reserve-data {
+      padding: 1rem;
+      border: solid 1px #ccc;
+      border-radius: 0.5rem;
+    }
+    &__personal-data {
+      padding: 1rem;
+      border: solid 1px #ccc;
+      border-radius: 0.5rem;
+    }
+    &__billing-data {
       padding: 1rem;
       border: solid 1px #ccc;
       border-radius: 0.5rem;
@@ -15,39 +25,54 @@
 <template>
   <form class="form-reserve" @submit.prevent="submit">
     <div>Reserva em :</div>
-    <div class="form-reserve__hotel-name ">{{ props.hotel.name }}</div>
-    <div class="form-reserve__data mt-4">
-      <div>De <b>{{ dates.start }} á {{ dates.end }}</b></div>
-      <div><b>{{ dates.daily }}</b> diária (s)</div>
-      <div>Valor por diária: <b>{{ formatCurrency(hotel.price) }}</b></div>
-      <div>Valor Total <b>{{ formatCurrency(hotel.price * dates.daily) }}</b></div>
+    <div class="form-reserve__hotel-name">{{ props.hotel.name }}</div>
+    <div class="form-reserve__reserve-data mt-4">
+        <small>Informações da reserva:</small>
+      <div>
+        De <b>{{ dates.start }} á {{ dates.end }}</b>
+      </div>
+      <div>
+        <b>{{ dates.daily }}</b> diária (s)
+      </div>
+      <div>
+        Valor por diária: <b>{{ formatCurrency(hotel.price) }}</b>
+      </div>
+      <div>
+        Valor Total <b>{{ formatCurrency(hotel.price * dates.daily) }}</b>
+      </div>
     </div>
-    <fieldset class="mt-4">
-      <div>
-        <SimpleValidateField
-          v-model="stateForm.name"
-          label="Nome"
-          id="field-nome"
-          dataTest="field-nome"
-          type="text"
-          placeholder="Nome"
-          required
-          fieldClass="customInput"
-        />
-      </div>
-      <div>
-        <SimpleValidateField
-          v-model="stateForm.email"
-          label="Email"
-          id="field-email"
-          dataTest="field-email"
-          type="text"
-          placeholder="E-mail"
-          required
-          fieldClass="customInput"
-        />
-      </div>
-    </fieldset>
+    <div class="form-reserve__personal-data mt-4">
+        <small>Dados pessoais:</small>
+      <fieldset class="mt-4">
+        <div>
+          <SimpleValidateField
+            v-model="stateForm.name"
+            label="Nome"
+            id="field-nome"
+            dataTest="field-nome"
+            type="text"
+            placeholder="Nome"
+            required
+            fieldClass="customInput"
+          />
+        </div>
+        <div>
+          <SimpleValidateField
+            v-model="stateForm.email"
+            label="Email"
+            id="field-email"
+            dataTest="field-email"
+            type="text"
+            placeholder="E-mail"
+            required
+            fieldClass="customInput"
+          />
+        </div>
+      </fieldset>
+    </div>
+    <div class="form-reserve__billing-data mt-4">
+        <small>Dados de pagamento:</small>
+    </div>
 
     <div class="mt-4">
       <button class="customPrimaryButton">Reservar</button>
